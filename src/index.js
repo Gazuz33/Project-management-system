c87118d24865bdfec9bbf035c1be18f38d5ea6a5
Gazuz33 <a4tonetak@gmail.com>
2022-03-28 23:01:31 +0300

    add: add condition copy of profile

--- src/index.js
+++ src/index.js
@@ -4,20 +4,11 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/redux-store";
 
-let rerenderEntireTree = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
