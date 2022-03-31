a3a4285e11a6da1e01eb0f991951980790078a3f
Gazuz33 <a4tonetak@gmail.com>
2022-03-31 16:25:57 +0300

    add: add condition copy of dialogs

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -12,11 +12,11 @@ const mapStateToProps = (state) => {
 };
 const mapDispatchToProps = (dispatch) => {
   return {
-    updateNewMessageBody: (dispatch) => {
-      dispatch(sendMessageCreator());
-    },
-    sendMessage: (body) => {
+    updateNewMessageBody: (body) => {
       dispatch(updateNewMessageBodyCreator(body));
+    },
+    sendMessage: () => {
+      dispatch(sendMessageCreator());
     },
   };
 };
