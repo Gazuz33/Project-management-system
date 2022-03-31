45daf09ad6132bb616e98b7226839b62fe002187
Gazuz33 <a4tonetak@gmail.com>
2022-03-31 18:48:35 +0300

    add: add Users UI and BLL

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,12 +8,15 @@ const Navbar = () => {
         <NavLink to ="/profile" className={setActive}>Profile</NavLink></div>
       <div className={s.item}>
         <NavLink to="/dialogs" className={setActive}>Messages</NavLink></div>
+        <div className={s.item}>
+        <NavLink to="/users" className={setActive}>Users</NavLink></div>
       <div className={s.item}>
         <NavLink to="/news" className={setActive}>News</NavLink></div>
       <div className={s.item}>
         <NavLink to="/music" className={setActive}>Music</NavLink></div>
       <div className={s.item}>
         <NavLink to="/settings" className={setActive}>Settings</NavLink></div>
+        
     </nav>
   ); 
 };
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,11 +1,13 @@
 import { combineReducers, createStore } from "redux";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
+import userReducer from "./users-reducer";
 
 let reducers = combineReducers({
-    profilePage: profileReducer,
-    dialogsPage: dialogsReducer
+  profilePage: profileReducer,
+  dialogsPage: dialogsReducer,
+  usersPage: userReducer,
 });
 let store = createStore(reducers);
 
-export default store; 
+export default store;
