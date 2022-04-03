0c936b34c73e9a0b5aa64f1a9b462cae73d60271
Gazuz33 <a4tonetak@gmail.com>
2022-04-03 23:54:12 +0300

    add: refactor with componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,13 @@ import userPhoto from "../../assets/images/userPhoto.png";
 import React from "react";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          this.props.setUsers(response.data.items);
-    });
-  }
+  componentDidMount() {
+    axios
+    .get("https://social-network.samuraijs.com/api/1.0/users")
+    .then((response) => {
+      this.props.setUsers(response.data.items);
+  });
+}
   render() {
     return (
       <div className={s.item}>
