9854144209b4154dd27cd4a6b4087dec6dda1e1b
Gazuz33 <a4tonetak@gmail.com>
2022-04-04 23:25:31 +0300

    add: add users pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,10 +1,19 @@
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
+import {
+  followAC,
+  setCurrentPageAC,
+  setUsersAC,
+  unfollowAC,
+  setUsersTotalCountAC,
+} from "../../redux/users-reducer";
 import Users from "./Users";
 
 let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
   };
 };
 let mapDispatchTpProps = (dispatch) => {
@@ -17,7 +26,13 @@ let mapDispatchTpProps = (dispatch) => {
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users));
-    }
+    },
+    setCurrentPage: (pageNumber) => {
+      dispatch(setCurrentPageAC(pageNumber));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setUsersTotalCountAC(totalCount));
+    },
   };
 };
 
