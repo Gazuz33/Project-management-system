9c6dfc95ffd3e5ecc7bbf590c7c627c41db7f95f
Gazuz33 <a4tonetak@gmail.com>
2022-04-22 21:58:32 +0300

    add: refactor status method with useEffect

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,13 @@
 import Preloader from "../../common/Preloader/Preloader";
 import s from "./ProfileInfo.module.css";
-import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 const ProfileInfo = (props) => {
   if (!props.profile ){
     return <Preloader /> 
   }
   return (
     <div>
-      <div className={s.item}><ProfileStatus status = {props.status} updateStatus={props.updateStatus} /></div>
+      <div className={s.item}><ProfileStatusWithHooks status = {props.status} updateStatus={props.updateStatus} /></div>
       <img src={props.profile.photos.large}></img>
       <div className={s.item}><h3>My posts</h3></div>
     </div>
