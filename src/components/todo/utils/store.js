54ed78c0328f76049efce900ba2f4d529dd1e883
Gazuz33 <a4tonetak@gmail.com>
2022-05-03 12:47:16 +0300

    add: fix styles

--- src/components/todo/utils/store.js
+++ src/components/todo/utils/store.js
@@ -1,15 +1,15 @@
 const cards = [
     {
       id: 'card-1',
-      title: 'Learning how to cook',
+      title: 'To make a presentation',
     },
     {
       id: 'card-2',
-      title: 'Making sandwich',
+      title: 'To write a scientific article',
     },
     {
       id: 'card-3',
-      title: 'Taking the trash out',
+      title: 'To make a Pull Request',
     },
   ];
   
